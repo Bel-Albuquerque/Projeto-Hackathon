18cc210efa4850c6cfe91e0e42ec893c000b515a
Juliana Braga <juliana4braga@gmail.com>
2021-08-23 21:12:42 -0300

    Adiciona fotos aleatoriamente no site

---
+++ script.js
@@ -0,0 +1,21 @@
+async function fetchImage() {
+  const number = Math.round(Math.random() * 9);
+  const index = Math.floor(Math.random() * 99);
+  const url = `https://picsum.photos/v2/list?page=${number}&limit=100`;
+  const result = await fetch(url);
+  const foto = await result.json();
+  return foto[index].download_url;
+}
+
+async function createImage() {
+  const imageSrc = await fetchImage();
+  const image = document.createElement('img');
+  image.classList.add('image');
+  image.src = imageSrc;
+  image.alt = 'Imagem bonita';
+  document.querySelector('#images').appendChild(image);
+}
+
+for (index = 0; index <= 9; index += 1) {
+  createImage();
+}
