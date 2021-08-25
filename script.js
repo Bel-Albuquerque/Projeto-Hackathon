060aed4057e47cbfe04fe25208d84a11dd61b5dc
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-08-24 22:38:34 -0300

    funçoes pra galeria dinamica etc

--- script.js
+++ script.js
@@ -11,12 +11,38 @@ async function fetchImage() {
 async function createImage() {
   const imageSrc = await fetchImage();
   const image = document.createElement('img');
-  image.classList.add('image');
+  image.classList.add('image_galery');
   image.src = imageSrc;
   image.alt = 'Imagem bonita';
-  document.querySelector('#images').appendChild(image);
+  document.querySelector('#galery_view').appendChild(image);
 }
 
 for (index = 0; index <= 19; index += 1) {
   createImage();
 } 
+
+
+function criarGaleria() {
+  document.querySelector('#galery_').style.display='flex'
+}
+
+document.querySelector("#idGaleria").addEventListener('click', criarGaleria)
+
+function fechaGaleria() {
+  document.querySelector('#galery_').style.display='none'
+}
+
+document.querySelector('#close').addEventListener('click', fechaGaleria)
+
+function confirmarEmail(){
+  document.querySelector('#emailConfirmation').style.display='inline';
+  document.querySelector('#inpuEmail').value=''
+  document.querySelector('#inpuEmail').placeholder='Cadastro confirmado'
+}
+
+document.querySelector('#inpuEmail').addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+     confirmarEmail()
+
+  }
+});
